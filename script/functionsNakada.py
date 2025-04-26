5b339abd9071ccf71aea93b39a8758b897fa2a3e
snakada1615 <khf04440@nifty.com>
2025-04-26 11:01:07 +0900

    getTableFromPDF function update

--- script/functionsNakada.py
+++ script/functionsNakada.py
@@ -4,6 +4,88 @@ from pandas.io.stata import StataReader
 import fitz
 
 
+stateNames1 = [
+    'Andhra Pradesh ',
+    'Arunachal Pradesh',
+    'Assam',
+    'Bihar',
+    'Chhattisgarh',
+    'Delhi',
+    'Goa',
+    'Gujarat',
+    'Haryana',
+    'Himachal Pradesh',
+    'Jharkhand',
+    'Karnataka',
+    'Kerala',
+    'Madhya Pradesh',
+    'Maharashtra',
+    'Manipur',
+    'Meghalaya',
+    'Mizoram',
+    'Nagaland',
+    'Odisha',
+    'Punjab',
+    'Rajasthan',
+    'Sikkim',
+    'Tamil Nadu',
+    'Telangana',
+    'Tripura',
+    'Uttar Pradesh',
+    'Uttarakhand',
+    'West Bengal',
+    'Andaman & N. Island',
+    'Chandigarh',
+    'Dadra & Nagar Haveli',
+    'Jammu & Kashmir',
+    'Ladakh',
+    'Lakshadweep',
+    'Puducherry',
+    'All-India'
+]
+
+stateNames2 = [
+    'Andhra Pradesh',
+    'Arunachal Pradesh',
+    'Assam',
+    'Bihar',
+    'Chhattisgarh',
+    'Delhi',
+    'Goa',
+    'Gujarat',
+    'Haryana',
+    'Himachal Pradesh',
+    'Jharkhand',
+    'Karnataka',
+    'Kerala',
+    'Madhya Pradesh',
+    'Maharashtra',
+    'Manipur',
+    'Meghalaya',
+    'Mizoram',
+    'Nagaland',
+    'Odisha',
+    'Punjab',
+    'Rajasthan',
+    'Sikkim',
+    'Tamil Nadu',
+    'Telangana',
+    'Tripura',
+    'Uttar Pradesh',
+    'Uttarakhand',
+    'West Bengal',
+    'A & N Islands',
+    'Chandigarh',
+    'Dadra & Nagar Haveli & Daman &',
+    'Dadra & Nagar Haveli',
+    'Jammu & Kashmir',
+    'Ladakh',
+    'Lakshadweep',
+    'Puducherry',
+    'all-India'
+]
+
+
 def listVariable(state_dir, return_file):
     # Directory containing your .dta files
     all_vars = []
@@ -44,17 +126,16 @@ def extractTextFromPDF(source, pages):
     return res
 
 
-# source = 'data_original/CAMS Report_October_N.pdf'
-# pages = [79,80,81]
-# source = 'data_original/Final_Report_HCES_2023-24L.pdf'
-# pages = [80,81,82]
-# result = extractTextFromPDF(source, pages)
-# print(result[81])
-
-def text_DataFrame(start_lineNumber, end_lineNumber, iterationCount):
-    res = []
-    
-    return res
+def extractTextFromPDF2(sourceNum, pages):
+    match sourceNum:
+        case 1: source = 'data_original/CAMS Report_October_N.pdf'
+        case 2: source = 'data_original/Final_Report_HCES_2023-24L.pdf'
+        case _: source = "none"
+    if source != "none":
+        return extractTextFromPDF(source, pages)
+    else:
+        print('select 1 for digital access, 2 for houshold status')
+        return False
 
 def formatText(myText, stateNames):
     lines = myText.strip().split('\n')
@@ -95,3 +176,28 @@ def is_number(string):
         return True
     except ValueError:
         return False
+
+def getTableFromPDF(sourceNum, pages):
+    match sourceNum:
+        case 1: 
+            source = 'data_original/CAMS Report_October_N.pdf'
+            stateName = stateNames2
+        case 2: 
+            source = 'data_original/Final_Report_HCES_2023-24L.pdf'
+            stateName = stateNames1
+        case _: source = "none"
+    if source == "none":
+        print('select 1 for digital access, 2 for houshold status')
+        return False
+    else:
+        pageContents = extractTextFromPDF(source, pages)
+        res = {}
+        for i in pages:
+            data = formatText(pageContents[i], stateName)
+            colCount = len(data[0])
+            colNames = [f'col{i}' for i in range(1, colCount+1)]
+            df = pd.DataFrame(data, columns=colNames)
+            res[i]=df
+
+        return res
+    
